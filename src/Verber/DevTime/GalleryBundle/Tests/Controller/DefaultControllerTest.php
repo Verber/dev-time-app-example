472412e49c69564c0a32d6a37c828ee2478929d4
Ivan Mosiev <i.k.mosev@gmail.com>
2013-12-17 03:19:03 +0200

    Added empty bundle

---
+++ src/Verber/DevTime/GalleryBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Verber\DevTime\GalleryBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/hello/Fabien');
+
+        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+    }
+}
---
+++ src/Verber/DevTime/GalleryBundle/VerberDevTimeGalleryBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Verber\DevTime\GalleryBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class VerberDevTimeGalleryBundle extends Bundle
+{
+}
